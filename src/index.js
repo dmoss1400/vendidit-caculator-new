e8f028f71db5c0781aded9fcdee8ab4c27ea3723
dmoss1400 <douglasmoss1400@gmail.com>
2025-06-22 13:58:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
+import App from "./App";  // Ensure this imports from App.js
+import "./index.css";  // Optional, if you have styles (but not necessary for now)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />  {/* The App component should be rendered here */}
+  </React.StrictMode>,
+  document.getElementById("root")  // Renders the app inside the <div id="root"></div> in index.html
+);
