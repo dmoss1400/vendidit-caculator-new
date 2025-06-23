ed7bd4dd92d00899f4b2508ea10f83d2086f4d8c
dmoss1400 <douglasmoss1400@gmail.com>
2025-06-23 08:25:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";  // Ensure this imports from App.js
-import "./index.css";  // Optional, if you have styles (but not necessary for now)
+import App from "./App";
+import "./index.css"; // Optional, if you have styles
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root")  // This must match <div id="root"></div> in index.html
 );
